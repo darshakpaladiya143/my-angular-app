b64c2c90badd303c062f81b84fb9d352ba392602
Shaishav-code <shd.narola@gmail.com>
2025-02-12 16:39:11 +0530

    latest_changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+       // Import the component instead of declaring it if its an standalone componant
       imports: [AppComponent],
     }).compileComponents();
   });
@@ -11,19 +12,19 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    // expect(app).to.be.true;
   });
 
-  it(`should have the 'my-angular-app' title`, () => {
+  it(`should have the title 'my-angular-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular-app');
+    expect(app.title).equal('my-angular-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular-app');
+    expect(compiled.querySelector('h1')?.textContent).contain('Hello, my-angular-app');
   });
 });
